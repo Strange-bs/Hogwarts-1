e478e457908f9a1e48f61de4b532388c3c1e1dac
Stephen Strange <80538114+Strange-bs@users.noreply.github.com>
2022-04-06 18:30:08 +0500

    first

--- hogwarts/main/views.py
+++ hogwarts/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import *
 from telepot import Bot
 
-bot = Bot('Your Bot Token')
+bot = Bot('5161954720:AAHh75M1B2wHysGNTDH3C8u1ZcXFTiDGQbU')
 
 def home_view(request):
     if request.method == 'POST':
@@ -12,14 +12,14 @@ def home_view(request):
             name = request.POST.get('full_name')
             email = request.POST.get('email')
             msg = request.POST.get('message')
-            bot.sendMessage('Your Id', f'Hey New Contact\n{name}\n{email}\n{msg}')
+            bot.sendMessage('1359290361', f'Hey New Contact\n{name}\n{email}\n{msg}')
             contactForm.save()
             return redirect('/')
         if registerForm.is_valid():
             name = request.POST.get('full_name')
             phone = request.POST.get('phone')
             degree = request.POST.get('degree')
-            bot.sendMessage('1060158414', f'Hey New Student\n{name}\n{phone}\n{degree}')
+            bot.sendMessage('1359290361', f'Hey New Student\n{name}\n{phone}\n{degree}')
             registerForm.save()
             return redirect('/')
     else:
